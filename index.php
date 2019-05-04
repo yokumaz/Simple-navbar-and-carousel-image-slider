fec242aa90a9aff8259f4ab9fdd862c12d3aa5ab
yokumaz <49632928+yokumaz@users.noreply.github.com>
2019-05-04 14:16:39 +0530

    Add files via upload
    file uploaded

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Your template</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/jquery-3.4.1.js"></script>
+	<script type="text/javascript" src="./js/popper.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<meta charset="UTF-8">
+</head>
+<body>
+<nav class="navbar bg-dark navbar-dark navbar-expand-sm" style="">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mynav">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a href="#" class="nav-brand">Logo</a>
+	<div class="collapse navbar-collapse" id="mynav">
+		<ul class="navbar-nav">
+			<li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+			<li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+			<li class="nav-item"><a href="#" class="nav-link">Contact us</a></li>
+		</ul>
+	</div>
+</nav>
+<center>
+<div id="imageslider" class="carousel slide" data-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+			<img src="img/img1.jpg" class="d-block w-20 img-fluid">
+		</div>
+		<div class="carousel-item">
+			<img src="img/img2.jpg" class="d-block w-20 img-fluid">
+		</div>
+		<div class="carousel-item">
+			<img src="img/img1.jpg" class="d-block w-20 img-fluid">
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#imageslider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#imageslider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</center>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+<br/><br/>
+</body>
+</html>
